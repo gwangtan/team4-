3c3efe59ba395c314aab3ac4278068dc7f5e1a9b
kwonsun212 <kwonsun212@gmail.com>
2025-05-18 16:54:09 +0900

    [Feat]동글 프리팹 랜덤 설정 및 드래그 앤 드롭 방식 변경,자잘한 버그 수정

--- Assets/Scripts/Dongle.cs
+++ Assets/Scripts/Dongle.cs
@@ -4,14 +4,22 @@ using UnityEngine;
 
 public class Dongle : MonoBehaviour
 {
+    public int level;
     public bool isDrag;
     Rigidbody2D rigid;
+    Animator anim;
 
     private void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
+
+    private void OnEnable()
+    {
+        anim.SetInteger("Level", level);
+    }
 
     // Update is called once per frame
     void Update()
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+
+    public Dongle lastDongle;
+    public GameObject donglePrefab;
+    public Transform dongleGroup;
+
+
+    private void Awake()
+    {
+        Application.targetFrameRate = 60;       //ÇÁ·¹ÀÓ(FPS) ¼³Á¤
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        NextDongle();
+    }
+    Dongle GetDongle()
+    {
+        GameObject instant = Instantiate(donglePrefab, dongleGroup);
+        Dongle instantDongle = instant.GetComponent<Dongle>();
+        return instantDongle;
+    }
+
+
+    void NextDongle()
+    {
+        Dongle newDongle = GetDongle();
+        lastDongle = newDongle;
+        lastDongle.level = Random.Range(0, 8); // ¸Æ½Ã¸Ø °ªÀº Æ÷ÇÔ ¾ÈµÊ 
+        lastDongle.gameObject.SetActive(true);
+
+        StartCoroutine("WaitNext");
+    }
+
+    IEnumerator WaitNext()
+    {
+        while (lastDongle != null)
+        {
+            yield return null;
+        }
+
+        yield return new WaitForSeconds(2.5f);
+
+        NextDongle();
+    }
+
+    public void TouchDown()
+    {
+        if(lastDongle == null)
+        {
+            return;
+        }
+
+        lastDongle.Drag();
+    }
+
+    public void TouchUp()
+    {
+        if (lastDongle == null)
+        {
+            return;
+        }
+
+        lastDongle.Drop();
+        lastDongle = null;
+    }
+}
