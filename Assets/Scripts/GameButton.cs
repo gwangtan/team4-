d1356c07300d2858d21bc28e9a855fbe0ae56011
gwangtan <>
2025-06-04 11:55:36 +0900

    도움말,설정 버튼 파넬

--- Assets/Scripts/GameButton.cs
+++ Assets/Scripts/GameButton.cs
@@ -6,9 +6,32 @@ using UnityEngine.SceneManagement;
 public class GameButton : MonoBehaviour
 {
 
+    public GameObject HelpPanel;
+    public GameObject SettingPanel;
+
     public void GameStartButtonAction()
     {
         SceneManager.LoadScene("SampleScene");
+    }
+
+    public void OpenPanel()
+    {
+        HelpPanel.SetActive(true);
+    }
+
+    public void CloseHelpPanel()
+    {
+        HelpPanel.SetActive(false);
+    }
+
+    public void OpenSetting()
+    {
+        SettingPanel.SetActive(true);
+    }
+
+    public void CloseSeeting()
+    {
+        SettingPanel.SetActive(false );
     }
     // Start is called before the first frame update
     void Start()
