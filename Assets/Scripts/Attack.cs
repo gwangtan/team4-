bec94d4b9c8af5a4172a5ff6a3529b210af5006e
kwonsun212 <kwonsun212@gmail.com>
2025-06-17 15:57:26 +0900

    [Feat]공격시 마나소모

--- Assets/Scripts/Attack.cs
+++ Assets/Scripts/Attack.cs
@@ -16,15 +16,23 @@ public class Attack : MonoBehaviour
 
     private void OnButtonClicked()
     {
-        if (player.target != null && !gameManager.isOver)
+        if (player.target != null && !gameManager.isOver && player.MP >= 1)
         {
             float damage = gameManager.score * 10f; // 점수 × 10
             player.target.TakeDamage(damage);
+            player.MP -= 1;
+
             Debug.Log($"[공격] 점수 {gameManager.score} × 10 = {damage} 데미지 입힘");
 
             gameManager.score = 0; // 점수 초기화
 
             gameManager.GameOver();
         }
+
+        else if (player.MP < 1)
+        {
+            Debug.Log("마나가 부족합니다! (MP < 1)");
+        }
+        
     }
 }
